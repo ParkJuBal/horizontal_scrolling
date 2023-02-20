25f80c8a8ebb80c108925e1a3d9eed2e8075784b
ParkJuBal <93561000+ParkJuBal@users.noreply.github.com>
2023-02-20 15:22:31 +0900

    Add files via upload

--- js/horizontal_scrolling.js
+++ js/horizontal_scrolling.js
@@ -1,46 +1,55 @@
 $(document).ready(function(){
-  function horizontalScrolling(){
-    const container = document.querySelector('.container');
-    let winW = document.body.offsetWidth; // 실제 사용하는 브라우저의 안쪽 너비
-    let winH = window.innerHeight; // 브라우저 두께를 제외한 높이
-    let contWidth = container.scrollWidth; // 컨테이너의 보이지않는 범위까지의 너비
-    
-    let contHeight = contWidth - winW + winH; // container width - 윈도우 너비 + 윈도우 높이값을 변수로 지정
-    container.style.height = contHeight + 'px'; // 위 변수를 container 높이값으로 지정
 
-    document.addEventListener('scroll', function() {
-      let containerOffTop = container.offsetTop; // 횡스크롤 시작점
-      let winTop = window.scrollY || document.documentElement.scrollTop; // 전자는 IE에서 작동하지 않음
-    
-      if(winTop >= containerOffTop && winTop <= (container.nextElementSibling.offsetTop) - winH){
-        // 윈도우탑이 container 영역에 왔을 때부터 윈도우 하단에서 container 다음 컨텐츠가 시작될 때까지 실행
-        container.firstElementChild.classList.add('fixed');
-    
-        let scrollValue = winTop - containerOffTop; // container 영역내에서의 스크롤휠 값(0부터 시작함)
-        container.firstElementChild.style.transform = 'translateX(' + (-scrollValue) + 'px)';
-        
-      } else if(winTop >= (container.nextElementSibling.offsetTop) - winH){
-        // 윈도우 하단에서 container 다음 컨텐츠가 시작될 때
-        container.firstElementChild.classList.remove('fixed');
-        container.firstElementChild.classList.add('fixed_off');
-    
-        // 화면이 container 영역 아래로 갔을 때 translateX값을 컨테이너 최우측값으로 맞춤
-        let contScDownInit = contWidth - winW;
-        container.firstElementChild.style.transform = 'translateX(' + (-contScDownInit) + 'px)';
-    
-      }else{
-        // container 영역 위로 올랐을 때
-        container.firstElementChild.classList.remove('fixed','fixed_off');
-        container.firstElementChild.style.transform = 'translateX(' + 0 + 'px)'; // 화면이 container 영역 위로 갔을 때 translateX값을 컨테이너 좌측값으로 맞춤
-      }
-
-    });
-  }
-  horizontalScrolling();
-  $(window).resize(function(){
-    window.location.reload()
+  $(window).on('load resize',function(){
+    horizontalScrolling();
+    // let timer;
+    // $(window).on('resize',function(){
+    //   clearTimeout(timer);
+    //   timer = setTimeout(function(){
+    //     horizontalScrolling();
+    //   },200)
+    // });
   });
+  
 });
+
+function horizontalScrolling(){
+  const container = document.querySelector('.container');
+  let winW = document.body.offsetWidth; // 실제 사용하는 브라우저의 안쪽 너비
+  let winH = window.innerHeight; // 브라우저 두께를 제외한 높이
+  let contWidth = container.scrollWidth; // 컨테이너의 보이지않는 범위까지의 너비
+  
+  let contHeight = contWidth - winW + winH; // container width - 윈도우 너비 + 윈도우 높이값을 변수로 지정
+  container.style.height = contHeight + 'px'; // 위 변수를 container 높이값으로 지정
+
+  document.addEventListener('scroll', function() {
+    let containerOffTop = container.offsetTop; // 횡스크롤 시작점
+    let winTop = window.scrollY || document.documentElement.scrollTop; // 전자는 IE에서 작동하지 않음
+  
+    if(winTop >= containerOffTop && winTop <= (container.nextElementSibling.offsetTop) - winH){
+      // 윈도우탑이 container 영역에 왔을 때부터 윈도우 하단에서 container 다음 컨텐츠가 시작될 때까지 실행
+      container.firstElementChild.classList.add('fixed');
+  
+      let scrollValue = winTop - containerOffTop; // container 영역내에서의 스크롤휠 값(0부터 시작함)
+      container.firstElementChild.style.transform = 'translateX(' + (-scrollValue) + 'px)';
+      
+    } else if(winTop >= (container.nextElementSibling.offsetTop) - winH){
+      // 윈도우 하단에서 container 다음 컨텐츠가 시작될 때
+      container.firstElementChild.classList.remove('fixed');
+      container.firstElementChild.classList.add('fixed_off');
+  
+      // 화면이 container 영역 아래로 갔을 때 translateX값을 컨테이너 최우측값으로 맞춤
+      let contScDownInit = contWidth - winW;
+      container.firstElementChild.style.transform = 'translateX(' + (-contScDownInit) + 'px)';
+  
+    }else{
+      // container 영역 위로 올랐을 때
+      container.firstElementChild.classList.remove('fixed','fixed_off');
+      container.firstElementChild.style.transform = 'translateX(' + 0 + 'px)'; // 화면이 container 영역 위로 갔을 때 translateX값을 컨테이너 좌측값으로 맞춤
+    }
+
+  });
+}
 
 // 제이쿼리 코드
 // $(document).ready(function(){
